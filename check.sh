1707e20b685efaf9f3fe2750f610e6311dd6903e
Andrey Romanov <andrey.a.romanov@gmail.com>
2016-07-28 15:20:51 +0300

    Some fixes

--- check.sh
+++ check.sh
@@ -18,15 +18,16 @@ batpath=$(dirname $0)
 
   echo $msg2
 
-  for file in $runpath/req/*/*
-  do
-    file_name=${file##*/}
-    file_name=${file_name%%.*}
+	for file in $runpath/req/*/*
+	do
+		file_name=${file##*/}
+		file_name=${file_name%%.*}
 
-   if [ ! -f $runpath/asn/$file_name.txt ];then
+		if [ ! -f $runpath/asn/$file_name.txt ];then
       dumpasn1 -p $file | sed "s/OBJECT IDENTIFIER '/OBJECT IDENTIFIER unknown (/g;s/'$/)/g" | sed ':l /OBJECT IDENTIFIER/s/\(([^ )]*\)[ ]/\1./;tl' > $runpath/asn/$file_name.txt
-   fi
-  done
+		else
+		fi
+	done
 
   echo $msg3
   echo "python $batpath/check.py $runpath"
